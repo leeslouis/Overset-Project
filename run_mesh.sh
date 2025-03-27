3b953d7c727f4cc9ed658dc9e1949dab0fe9c3a9
leeslouis <louislees99@gmail.com>
2025-03-27 15:43:36 +0000

    Update run_mesh.sh

--- run_mesh.sh
+++ run_mesh.sh
@@ -3,18 +3,9 @@
 foamCleanTutorials
 
 cd overset
-surfaceFeatureExtract
 blockMesh
 topoSet -dict system/topoSetDictR1
-refineMesh -dict system/refineMeshDict1 -overwrite
-topoSet -dict system/topoSetDictR2
-refineMesh -dict system/refineMeshDict2 -overwrite
-decomposePar
-mpirun -oversubscribe -np 16 snappyHexMesh -overwrite -parallel | tee log.snappyHexMesh
-topoSet -dict system/topoSetDictR2
-refineMesh -dict system/refineMeshDict2 -overwrite
 snappyHexMesh -overwrite | tee log.snappyHexMesh
-createPatch -overwrite
 cd ..
 
 cd background
@@ -28,5 +19,4 @@ setFields | tee log.setFields
 patchSummary
 decomposePar
 renumberMesh -overwrite
-mpirun -oversubscribe -np 16 overPimpleDyMFoam -parallel | tee log.overPimpleDyMFoam
-
+mpirun -np 16 overPimpleDyMFoam -parallel | tee log.overPimpleDyMFoam
